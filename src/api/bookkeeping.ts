a68ffb14308caac5a516959d0bfca1130733ff84
Wray <wray20156294@gmail.com>
2025-01-23 15:58:54 +0800

    update api prefix

--- src/api/bookkeeping.ts
+++ src/api/bookkeeping.ts
@@ -3,25 +3,25 @@ import type * as BookkeepingType from "@/types/bookkeeping";
 
 // 记账记录分页列表
 export const queryBookkeepingPage = (bookkeepingListDto: BookkeepingType.BookkeepingPageDto) => {
-    return request.post('/bookkeeping-service/records/page', bookkeepingListDto);
+    return request.post('/bookkeeping-service/bookkeepingRecords/page', bookkeepingListDto);
 }
 
 // 新增记账记录
 export const addBookkeeping = (bookkeepingAddDto: BookkeepingType.BookkeepingAddDto) => {
-    return request.post('/bookkeeping-service/records/add', bookkeepingAddDto)
+    return request.post('/bookkeeping-service/bookkeepingRecords/add', bookkeepingAddDto)
 }
 
 // 修改记账记录
 export const updateBookkeeping = (bookkeepingUpdateDto: BookkeepingType.BookkeepingUpdateDto) => {
-    return request.put('/bookkeeping-service/records/update', bookkeepingUpdateDto)
+    return request.put('/bookkeeping-service/bookkeepingRecords/update', bookkeepingUpdateDto)
 }
 
 // 删除记账记录
 export const deleteBookkeeping = (id: number) => {
-    return request.delete('/bookkeeping-service/records/delete?id=' + id)
+    return request.delete('/bookkeeping-service/bookkeepingRecords/delete?id=' + id)
 }
 
 // 查询记账记录详情
 export const queryBookkeepingDetail = (id: number) => {
-    return request.get('/bookkeeping-service/records/detail?id=' + id)
+    return request.get('/bookkeeping-service/bookkeepingRecords/detail?id=' + id)
 }
